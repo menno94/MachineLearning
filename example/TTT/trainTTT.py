889e4da1beffa464b76a4a42a903280947f8a8fa
menno94 <menno-deridder@hotmail.com>
2019-03-25 21:09:26 +0100

    Analyse added

--- example/TTT/trainTTT.py
+++ example/TTT/trainTTT.py
@@ -8,20 +8,32 @@ from envTTT import env_TTT
 if __name__ =='__main__':
     env = env_TTT()
     Q = Q_agent(env)
+    Q.analyse = True
     now = datetime.datetime.now()
-    Q.create_model(N = [10],
-                   learning_rate = 1e-3)
-    Q.train(episodes            =   500,
+    Q.create_model(N = [100],
+                   learning_rate = 1e-4)
+    Q.train(episodes            =   5000,
             epsilon             =   1, 
             epsilon_min         =   0.02,
             epsilon_decay       =   0.97, 
             batch_size          =   32, 
             gamma               =   0.55,
-            memory_length       =   100,
-            breaks              =   50)
+            memory_length       =   500,
+            breaks              =   10)
     
     Q.save_model('TTT.h5')
     
     dt = (datetime.datetime.now()-now).total_seconds()
     print('Total time: {}'.format(dt))
+    
+    state = np.load('state.npy')
+    plt.figure()
+    plt.subplot(2,1,1)
+    plt.pcolor(state[0].reshape((3,3))/np.sum(state[0]))
+    plt.colorbar()
+    plt.subplot(2,1,2)
+    plt.pcolor(state[1].reshape((3,3))/np.sum(state[1]))
+    plt.colorbar()    
+    
+    
     
